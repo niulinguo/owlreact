dbb0085a7c948de2d64ec07f4fef22b85174cd41
lingo <niulinguo@163.com>
2020-09-06 11:27:40 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonSize, ButtonType } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            alert(123);
+          }}
         >
-          Learn React
-        </a>
+          Hello
+        </Button>
+        <Button autoFocus>Hello</Button>
+        <Button disabled>Hello Disabled</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Primary Large
+        </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+          Primary Small
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>
+          Danger Large
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+          Danger Small
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href={'https://www.baidu.com'}
+          target={'_blank'}
+        >
+          Baidu Link
+        </Button>
+        <Button
+          disabled
+          btnType={ButtonType.Link}
+          href={'https://www.baidu.com'}
+        >
+          Baidu Link Disabled
+        </Button>
       </header>
     </div>
   );
